20aa074725ee2e03819ddebac4a89c464e58e7a0
Anna Winther <annawinther@annas-mbp.lan>
2019-12-08 17:31:24 +0000

    added authRouter to server.js

--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const authRouter = require('../api/routes/authRoutes');
 
 const server = express();
 
 server.use(express.json());
 server.use(cors())
-// server.use('/api');
+server.use('/api', authRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json("Server running")

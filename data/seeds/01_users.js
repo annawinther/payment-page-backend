e1a4e76e2cb165055bff9a7b8a14a81dc8d8fd4e
Anna Winther <annawinther@annas-mbp.lan>
2019-12-08 14:31:16 +0000

    seeds modified

--- data/seeds/01_users.js
+++ data/seeds/01_users.js
@@ -7,7 +7,8 @@ exports.seed = function(knex) {
       return knex('users').insert([
         { id: 1, email: 'test@example.com', password: 'test1234', name: 'Test' },
         { id: 2, email: 'anna@example.com', password: 'anna1234', name: 'Anna' },
-        { id:3,  email: 'peter@example.com', password: 'peter1234', name: 'Peter' }
+        { id: 3,  email: 'peter@example.com', password: 'peter1234', name: 'Peter' }
       ]);
     });
 };
+ 
